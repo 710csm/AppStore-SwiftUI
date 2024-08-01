61d50440896ae6b0913e6d74537f8120711161d1
ChoiSeungMyeong <710csm@megastudy.net>
2024-08-01 11:16:40 +0900

    🚀 프로젝트 생성

---
+++ iOS-AppStore-SwiftUI/iOS-AppStore-SwiftUI/Sources/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS-AppStore-SwiftUI
+//
+//  Created by 최승명 on 8/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS-AppStore-SwiftUI/iOS-AppStore-SwiftUI/Sources/iOS_AppStore_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_AppStore_SwiftUIApp.swift
+//  iOS-AppStore-SwiftUI
+//
+//  Created by 최승명 on 8/1/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_AppStore_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
